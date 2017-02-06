ac6ed1c8f80cad4b6acd63b50dfa84e2c2e552d2
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-02-06 10:32:56 -0500

    2/06/17 changes, added location

--- compSci/src/Point.java
+++ compSci/src/Point.java
@@ -6,28 +6,68 @@ public class Point {
 		// TODO Auto-generated method stub
 
 	}
-	//parameterless constructor for objects of class point
-	public Point(){
-		
-		
-		
+
+	// parameterless constructor for objects of class point
+	public Point() {
+
 	}
-	//constructor with parameters for objects of class point
-	public Point(double x1, double y1){
-		//initialise instance variables
+
+	// constructor with parameters for objects of class point
+	public Point(double x1, double y1) {
+		// initialise instance variables
 		x = x1;
 		y = y1;
-		
+
 	}
-	public String toString(){
+
+	public String toString() {
 		return "(" + x + ", " + y + ")";
-		
+
 	}
-	public double getX(){
+
+	public double getX() {
 		return x;
 	}
-	public double getY(){
+
+	public double getY() {
 		return y;
-		
+
 	}
+
+	// Mutator for x
+	public double setX(double x1) {
+		return x1;
+	}
+
+	// Mutator for y
+	public double setY(double y1) {
+		return y1;
+
+	}
+
+	public boolean equals(Point p) {
+		return (p.x == x && p.y == y);
+
+	}
+	public void location(){
+		if(x > 0 && y > 0){
+			System.out.println("Quadrant I");
+		}
+		else if(x < 0 && y > 0){
+			System.out.println("Quadrant II");
+		}
+		else if(x < 0 && y < 0){
+			System.out.println("Quadrant III");
+		}
+		else if(x > 0 && y < 0){
+			System.out.println("Quadrant IV");
+		}
+		else if (x == 0){
+			System.out.println("x-axis");
+		}
+		else if (y == 0){
+			System.out.println("y-axis");
+		}
+	}
+
 }
