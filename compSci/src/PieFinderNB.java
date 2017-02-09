c3dd6f05639d44790e884e004bee3825909211be
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-02-09 10:30:14 -0500

    2/09/17 - added nested for loops to narrow down search

--- compSci/src/PieFinderNB.java
+++ compSci/src/PieFinderNB.java
@@ -1,10 +1,29 @@
-
+/**
+ * Your name date Estimating target and e Project
+ */
 
 public class PieFinderNB {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double target = Math.PI;
+		int numer, denom, bestNumer = 22, bestDenom = 7;
+		double minError = Math.abs((double) bestNumer / bestDenom - target);
+		denom = 8;
+		for (numer = 24; numer / denom > 3 && numer / denom < 4; numer++) {
+			for (denom = 8; denom <= 999; denom++) {
+				while (numer / denom == target && bestDenom < 999) {
+					
+				}
+			}
+		}
+		
+
+		target = Math.E;
+		bestNumer = 19;
+		bestDenom = 7;
+		minError = Math.abs((double) bestNumer / bestDenom - target);
+
+		// rest of e program goes here
 
 	}
-
 }
