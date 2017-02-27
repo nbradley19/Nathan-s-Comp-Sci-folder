8eb81a341d1d0b5aba313e0dfec0a38c1e68c72d
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-02-27 10:31:10 -0500

    added class Circle

---
+++ compSci/src/Circle.java
@@ -0,0 +1,20 @@
+
+
+public class Circle {
+
+	public Circle(Point p, double r) {
+		//constructor
+		
+	}
+	
+	public String toString(){
+		return "";
+	}
+	public double area(double r){
+		return Math.pow((Math.PI * r), 2);
+	}
+	public double circumference(double r){
+		return (2 * Math.PI * r);
+	}
+
+}
