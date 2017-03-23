7015aca4f1b1c105f04408d83c7b07c761244371
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-03-23 09:52:32 -0400

    made generatedNums work

--- compSci/src/compSci/GeneratedNums.java
+++ compSci/src/compSci/GeneratedNums.java
@@ -3,7 +3,15 @@ package compSci;
 public class GeneratedNums {
 
 	public static void main(String[] args) {
-		int nums[] = new int[101]; 
+		int nums[] = new int[101];
+		System.out.println("Index\tGenerated Number");
+		int sumDigits[] = new int[101];
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = i;
+			int sumDigits2 = i + (i / 10) + (i % 10);
+			sumDigits[i] = sumDigits2;
+			System.out.println(nums[i] + "\t " + sumDigits[i]);
+		}
 
 	}
 
