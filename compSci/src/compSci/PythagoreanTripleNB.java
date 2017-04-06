a74f3236e65cd13d024aa666ff5c6205d047dab7
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-04-06 10:28:17 -0400

    added all of the triples to an arrayList in PythagoreanTripleNB

--- compSci/src/compSci/PythagoreanTripleNB.java
+++ compSci/src/compSci/PythagoreanTripleNB.java
@@ -1,18 +1,29 @@
 package compSci;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class PythagoreanTripleNB {
 
 	public static void main(String[] args) {
 		int a, b, c;
+		
+		ArrayList<int[]> triples = new ArrayList<int[]>();
 		for (a = 1; a <= 100; a++) {
 			for (b = a + 1; b <= 100; b++) {
 				for (c = b + 1; c <= 100; c++) {
 					if ((a * a) + (b * b) == (c * c)) {
-						System.out.println(a + ", " + b + ", " + c);
+						// System.out.println(a + ", " + b + ", " + c);
+						int triple[] = {a, b, c};
+						triples.add(triple);
 					}
 
 				}
 			}
+		}
+		for(int[] i: triples){
+			System.out.println(Arrays.toString(i));
 		}
 	}
 
