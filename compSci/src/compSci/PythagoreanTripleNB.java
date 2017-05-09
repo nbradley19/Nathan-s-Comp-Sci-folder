a578720df75a1487c14c6b7b50ab8ad6aeab2136
nbradley19 <nbradley19@HSMATHE5450-03.hflcampus.monroe.edu>
2017-05-09 10:02:36 -0400

    Made PythagoreanTripleNB work

--- compSci/src/compSci/PythagoreanTripleNB.java
+++ compSci/src/compSci/PythagoreanTripleNB.java
@@ -1,5 +1,6 @@
 package compSci;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,6 +9,8 @@ public class PythagoreanTripleNB {
 
 	public static void main(String[] args) {
 		int a, b, c;
+		int d, e, f;
+		int[] merp = {1, 2, 3};
 		
 		ArrayList<int[]> triples = new ArrayList<int[]>();
 		for (a = 1; a <= 100; a++) {
@@ -17,14 +20,24 @@ public class PythagoreanTripleNB {
 						// System.out.println(a + ", " + b + ", " + c);
 						int triple[] = {a, b, c};
 						triples.add(triple);
+						for(int i = triples.size() - 1; i > 0; i--){
+							merp = triples.get(i - 1);
+							if((1.0 * triple[0] / triple[1]) == 1.0 * merp[0] / merp[1]  ){
+								triples.remove(triples.size() - 1);
+							}
+						}
 					}
 
 				}
 			}
 		}
+		
+		
 		for(int[] i: triples){
 			System.out.println(Arrays.toString(i));
 		}
+		
+		
 	}
 
 }
