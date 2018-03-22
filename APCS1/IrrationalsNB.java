5ca2266e528f7f0616907d9fbff97b27ec25dc60
natha <natha@10.24.164.174>
2018-03-21 20:45:16 -0400

    Added reprompt

--- APCS1/IrrationalsNB.java
+++ APCS1/IrrationalsNB.java
@@ -6,42 +6,78 @@ public class IrrationalsNB {
 	 * @author nbradley19
 	 * @param args
 	 */
-	private int denom;
-	private int numer;
-	private double target;
-	private double pi = Math.PI;
 
 	public static void main(String[] args) {
+		ArrayList<Rational> list = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a rational value (quit to quit)");
+		String in = sc.nextLine();
+		while(!in.equalsIgnoreCase("quit")) {
+			list = finder(in, .000001);
+			System.out.println(list.get(list.size() - 1));
+			System.out.println("Enter a rational value");
+			in = sc.nextLine();	
+		}
+		
 
 	}
 
-	public ArrayList<Rational> finder(String input, double error) {
+	public void runner() {
+
+	}
+
+	public static ArrayList<Rational> finder(String input, double error) {
 		ArrayList<Rational> list = new ArrayList<>();
+		double pi = Math.PI;
+		double e = Math.E;
+		int numer;
+		int denom;
 		if (input.equalsIgnoreCase("pi")) {
 			numer = 3;
 			denom = 1;
-			target = numer / denom;
-			while (!plusOrMinus(target, pi, error)) {
+			while (!plusOrMinus(numer / (denom * 1.0), pi, error)) {
+				if (check(numer, denom, pi)) {
+					denom++;
+				} else {
+					numer++;
+				}
 				list.add(new Rational(numer, denom));
-
 			}
+		} else if (input.equalsIgnoreCase("e")) {
+			numer = (int) e;
+			denom = 1;
+			while (!plusOrMinus(numer / (denom * 1.0), e, error)) {
+				if (check(numer, denom, e)) {
+					denom++;
+				} else {
+					numer++;
+				}
+				list.add(new Rational(numer, denom));
+			}
+		}
+		else {
+			int indexSlash;
+			input.replaceAll(" ", "");
+			indexSlash = input.indexOf("/");
+			int firstNum = Integer.parseInt(input.substring(0, indexSlash));
+			int secondNum = Integer.parseInt(input.substring(indexSlash, input.length()));
+			
 		}
 		return list;
 	}
 
-	public boolean plusOrMinus(double input, double out, double error) {
-		if (input + error == out || input - error == out) {
+	public static boolean plusOrMinus(double input, double out, double error) {
+		if (input + error > out && input - error < out) {
 			return true;
 		}
 		return false;
 	}
-	public Rational testFinder(int numer, int denom) {
-		if(numer / denom == pi - .001) {
-			return new Rational(numer, denom);	
+
+	public static boolean check(int numer, int denom, double out) {
+		if (numer / (denom * 1.0) > out) {
+			return true;
 		}
-		return new Rational(numer, denom);
-		
+		return false;
 	}
 
 }
