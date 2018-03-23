d36f688c37941150d8d130333d7c163f4a927e5e
nbradley19 <nbradley19@HSMATHE5450-07.hflcampus.monroe.edu>
2018-03-23 11:30:59 -0400

    made it werk

--- APCS1/IrrationalsNB.java
+++ APCS1/IrrationalsNB.java
@@ -6,15 +6,13 @@ public class IrrationalsNB {
 	 * @author nbradley19
 	 * @param args
 	 */
-
 	public static void main(String[] args) {
 		ArrayList<Rational> list = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a rational value (quit to quit)");
 		String in = sc.nextLine();
 		while(!in.equalsIgnoreCase("quit")) {
-			list = finder(in, .000001);
-			System.out.println(list.get(list.size() - 1));
+			list = finder(sc, in, .000001);
 			System.out.println("Enter a rational value");
 			in = sc.nextLine();	
 		}
@@ -26,12 +24,13 @@ public class IrrationalsNB {
 
 	}
 
-	public static ArrayList<Rational> finder(String input, double error) {
+	public static ArrayList<Rational> finder(Scanner _sc, String input, double error) {
 		ArrayList<Rational> list = new ArrayList<>();
 		double pi = Math.PI;
 		double e = Math.E;
 		int numer;
 		int denom;
+		input.replaceAll(" " , "");
 		if (input.equalsIgnoreCase("pi")) {
 			numer = 3;
 			denom = 1;
@@ -43,6 +42,10 @@ public class IrrationalsNB {
 				}
 				list.add(new Rational(numer, denom));
 			}
+			System.out.println("Your Fraction: " + list.get(list.size() - 1));
+			System.out.println("Pi is: " + pi);
+			System.out.println("Your Number: " + numer / (denom * 1.0));
+			System.out.println("Error: " + Math.abs((numer / (denom * 1.0) - pi)));
 		} else if (input.equalsIgnoreCase("e")) {
 			numer = (int) e;
 			denom = 1;
@@ -54,14 +57,30 @@ public class IrrationalsNB {
 				}
 				list.add(new Rational(numer, denom));
 			}
+			System.out.println("Your Fraction: " + list.get(list.size() - 1));
+			System.out.println("e is: " + e);
+			System.out.println("Your Number: " + numer / (denom * 1.0));
+			System.out.println("Error: " + Math.abs((numer / (denom * 1.0) - e)));
 		}
-		else {
-			int indexSlash;
-			input.replaceAll(" ", "");
-			indexSlash = input.indexOf("/");
-			int firstNum = Integer.parseInt(input.substring(0, indexSlash));
-			int secondNum = Integer.parseInt(input.substring(indexSlash, input.length()));
-			
+		else if(input.equalsIgnoreCase("sqrt") || input.equalsIgnoreCase("squareroot")) {
+			System.out.println("What number would you like to take the square root of?");
+			input = _sc.nextLine();
+			double sqrtIn = Double.parseDouble(input);
+			double finalSqrt = Math.sqrt(sqrtIn);
+			numer = (int)sqrtIn;
+			denom = 1;
+			while(!plusOrMinus(numer / (denom * 1.0), finalSqrt, error)) {
+				if (check(numer, denom, finalSqrt)) {
+					denom++;
+				} else {
+					numer++;
+				}
+				list.add(new Rational(numer, denom));
+			}
+			System.out.println("Your Fraction: " + list.get(list.size() - 1));
+			System.out.println("The Square Root of " + input + " is: " + finalSqrt);
+			System.out.println("Your Number: " + numer / (denom * 1.0));
+			System.out.println("Error: " + Math.abs((numer / (denom * 1.0) - finalSqrt)));
 		}
 		return list;
 	}
